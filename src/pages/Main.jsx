806b0fa0b1ae4dc7b4f00ec3af5192347ac4c983
Rodner Batista <orodnerbis1@gmail.com>
2025-11-06 23:41:31 -0400

    Refactor button rendering in Main page to use Header component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,22 +2,22 @@ import { useState } from 'react'
 import './Main.css'
 import ShoppingCart from '../assets/ShoppingCart.jsx';
 import PlusIcon from '../assets/PlusIcon.jsx';
+import Header from '../components/Header.jsx';
 function Main() {
+    const principalBtn = {
+        icon: ShoppingCart,
+        text: 'Nueva Venta'
+    }
+    const secundaryBtn = {
+        icon: PlusIcon,
+        text: 'Nuevo Producto'
+    }
     return (
         <main className='main'>
-            <header className='header'>
-                <h1>Dashboard</h1>
-                <div className="btn-container">
-                    <button className='btn btn-principal'>
-                        <ShoppingCart/>
-                        Nueva Venta
-                        </button>
-                    <button className='btn btn-secundario'>
-                        <PlusIcon/>
-                        Nuevo Producto
-                        </button>
-                    </div>
-            </header>
+            <Header title="DashBoard" 
+            principalBtn={principalBtn} 
+            secundaryBtn={secundaryBtn}
+            />
         </main>
     )
 
