255b8904a8d2f6de63f7e542174197dfa6b1dc22
Rodner Batista <orodnerbis1@gmail.com>
2025-11-06 23:41:32 -0400

    Introduce custom NotFound page and route handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Main from './pages/Main'
 import Layout from './layout/Layout.jsx'
+import NotFound from './pages/404.jsx'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Layout />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="*" element={<h1>Error 404: Página no encontrada</h1>} />
+          <Route path="*" element={<NotFound/>} />
         </Routes>
       </Router>
     </>
